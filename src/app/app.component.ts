e2614573b871755154a39cb7616416d26c0d1448
Sekkung <108264350+Sekkung@users.noreply.github.com>
2023-02-10 23:30:49 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component ,Inject,OnInit,Renderer2} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Angular-LAB';
+  theme:Theme = 'dark-theme';
+  constructor (
+    @Inject(DOCUMENT) private document:Document,
+    private renderer:Renderer2
+  ){}
+  ngOnInit() {
+    this.initializetheme;
+  }
+
+  switchTheme(){
+    this.document.body.classList.replace(this.theme,
+      this.theme === 'dark-theme'
+      ? (this.theme = 'light-theme')
+      : (this.theme = 'dark-theme')
+    )
+  }
+
+  initializetheme = ():void=>
+    this.renderer.addClass(this.document.body,this.theme);
 }
+export type Theme = 'light-theme' | 'dark-theme';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
